e65e04753fcb22e8c93702006bfeaeb6dedda026
Erika Chiba <19531669+InoriTakenaka@users.noreply.github.com>
2026-01-14 17:37:18 +0800

    feat(ui): update app's side bar menu and title bar to provide better user experience

--- Scheri.PETPanel/UIComponents/ViewModels/StatusPanel.cs
+++ Scheri.PETPanel/UIComponents/ViewModels/StatusPanel.cs
@@ -1,13 +1,29 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Data.Converters;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Scheri.PETPanel.UIComponents.ViewModels
-{
+namespace Scheri.PETPanel.UIComponents.ViewModels;
+
     public partial class StatusPanel:ObservableObject
     {
         [ObservableProperty]
         private bool _isConnected = true;     
+    }
+
+public class BoolToBrushConverter: IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Avalonia.Media.Brushes.Green : Avalonia.Media.Brushes.Red;
+        }
+        return Avalonia.Media.Brushes.Yellow;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
     }
 }
