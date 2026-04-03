4703aaf570abd70b03371e8bac693cd0cc8af101
Erika Chiba <19531669+InoriTakenaka@users.noreply.github.com>
2026-04-03 17:08:03 +0800

    refactor: reorganize HomeView code & add StatusCardViewModel - Refactor HomeView implementation - Add shared ViewModel for StatusCard to unify device status monitoring & update logic - Support consistent status handling across different device connections

--- Scheri.PETPanel/Features/Home/HomeViewModel.cs
+++ Scheri.PETPanel/Features/Home/HomeViewModel.cs
@@ -10,10 +10,14 @@ namespace Scheri.PETPanel.Features;
 
 public partial class HomeViewModel:ObservableObject
 {
+    public DeviceStatusViewModel PlcStatus { get; } = new("PLC Status");
+    public DeviceStatusViewModel CameraStatus { get; } = new("Camera Status");
+
     [RelayCommand]
     private void Navigate(Type? viewType)
     {
         if (viewType == null) return;
         WeakReferenceMessenger.Default.Send(new NavigateTypeMessage(viewType));
     }
+
 }
---
+++ Scheri.PETPanel/UIComponents/StatusCard/DeviceStatusViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Media;
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Scheri.PETPanel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheri.PETPanel.UIComponents;
+
+public partial class DeviceStatusViewModel:ViewModelBase
+{
+    [ObservableProperty]
+    private string _header;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StatusText))]
+    [NotifyPropertyChangedFor(nameof(StatusColor))]
+    private bool _isConnected;
+    private DispatcherTimer _statusUpdateTimer;
+    private readonly Func<bool> _statusIndicatorFunc;
+    public string StatusText => IsConnected ? "Online" : "Offline";
+    public Color StatusColor => IsConnected ? Color.Parse("#50fa7b") : Colors.Red;
+
+    public void UpdateDeviceStatus()
+    {
+        IsConnected = _statusIndicatorFunc.Invoke();
+    }
+
+    public DeviceStatusViewModel(string header, Func<bool> statusIndicatorFunc, bool initialStatus=false)
+    {
+        Header = header;
+        IsConnected = initialStatus;
+        _statusIndicatorFunc = statusIndicatorFunc;
+        _statusUpdateTimer = new DispatcherTimer {
+            Interval = TimeSpan.FromSeconds(2)
+        };
+        _statusUpdateTimer.Tick += (s, e) => UpdateDeviceStatus();
+        _statusUpdateTimer.Start();
+    }
+}
