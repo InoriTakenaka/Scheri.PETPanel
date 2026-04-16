2faa0d876f961d0324b67615ef378f97821639cc
Erika Chiba <19531669+InoriTakenaka@users.noreply.github.com>
2026-04-16 14:46:48 +0800

    build: update project compile options for both desktop & android target

--- Scheri.PETPanel/PETPanelMenuConfig.cs
+++ Scheri.PETPanel/PETPanelMenuConfig.cs
@@ -9,11 +9,11 @@ public class PETPanelMenuConfig : IMenuConfig
     public List<Navigate> GetMenuItems()
     {
         return new List<Navigate> {
-            new("Overview", Icon.Home, typeof(SystemOverview)),
-            new("Device", Icon.DeviceMeetingRoom, typeof(DeviceInfo)),
-            new("Camera", Icon.ScanCamera, typeof(CameraInfo)),
-            //new("Settings", Icon.Settings, typeof(SystemSetting)),
-            new("Logs", Icon.BookDatabase, typeof(SystemLog))
+            new("Overview", Icon.Home, NavigateType.Home),
+            new("Device", Icon.DeviceMeetingRoom, NavigateType.Device),
+            new("Camera", Icon.ScanCamera, NavigateType.Camera),
+            new("Settings", Icon.Settings, NavigateType.Settings),
+            new("Logs", Icon.BookDatabase, NavigateType.Logs)
         };
     }
 }
